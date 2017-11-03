8107ad8266c98f3c26807bb75fae1c3a42d6ef22
Akash Nagwanshi <akashnagwanshi@gmail.com>
2017-11-03 08:00:53 +0530

    Add files via upload

---
+++ predictRules/predictRules/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// function list
+_calculateSupport()
+_calculateConfidence()
+_generateRules()
+_sort()
+_queue()
+// driver code
+int
+main(int argc,char *argv[])
+{
+return 0;
+}
---
+++ predictRules/predictRules/main_cpp.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+class pItemAndRules{
+  public:
+// function list
+void _calculateSupport();
+void _calculateConfidence();
+char* _generateRules();
+void _sort();
+void _queue();
+
+};
+
+void pItemAndRules::_calculateConfidence(){
+
+}
+void pItemAndRules::_calculateSupport(){
+
+}
+
+// driver code
+int
+main(int argc,char *argv[]){
+return 0;
+}
